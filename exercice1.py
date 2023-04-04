7a168b9afb0870ff5951bb9bc65c8053b50ff6a8
NATHALIA FARIAS <nathaliafarias1095@gmail.com>
2023-04-04 15:55:33 +0200

    MAJ Pays + capitale + population + area

--- exercice1.py
+++ exercice1.py
@@ -6,10 +6,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 for country in soup.find_all('div', class_='col-md-4 country'):
-  print(country.find('h3').text)
-
-for capital in soup.find_all('div', class_='country-capital'):
-  print(capital.find('span').text)
-
-for population in soup.find_all('div', class_='country-population'):
-  print(population.find('span').text)
+  print(country.find('h3', class_='country-name').text)
+  print(country.find('span', class_='country-capital').text)
+  print(country.find('span', class_='country-population').text)
+  print(country.find('span', class_='country-area').text)
