f759b875f515e8fd86ee0af008d0c05a86d3b21b
NATHALIA FARIAS <nathaliafarias1095@gmail.com>
2023-04-04 16:11:41 +0200

    Saut de ligne

--- exercice1.py
+++ exercice1.py
@@ -6,7 +6,12 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 for country in soup.find_all('div', class_='col-md-4 country'):
-  print(country.find('h3', class_='country-name').text)
+    
+  pays = country.find('h3', class_='country-name').text
+  pays_espace = pays.lstrip()
+
+  print(pays_espace)
   print(country.find('span', class_='country-capital').text)
   print(country.find('span', class_='country-population').text)
   print(country.find('span', class_='country-area').text)
+  print("\n")
